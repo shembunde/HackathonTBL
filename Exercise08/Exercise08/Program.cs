0f58f9ea079a3dcd9b6cf440b7dbf0952bc807eb
Shem <33216815+shembunde@users.noreply.github.com>
2021-07-31 17:31:42 +0300

    Add files via upload

---
+++ Exercise08/Exercise08/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Data Source =DESKTOP-7K60JLI\SQLEXPRESS; Initial Catalog = Northwind; Integrated Security = True";
+
+            string query = "Select CompanyName " + Environment.NewLine +
+                    "from Customers " + Environment.NewLine;
+
+            Console.WriteLine("Enter City Name:");
+            int city = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+
+            using (SqlConnection sqlcon1 = new SqlConnection(connectionString))
+            {
+                sqlcon1.Open();
+                using (SqlCommand sql1 = new SqlCommand(query, (sqlcon1)))
+                {
+                    sql1.Parameters.AddWithValue("@city", city);
+                    using (SqlDataReader reader = sql1.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                Console.Write("Company names: {0}\nCompany Name \n\n", reader[0]);
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("no results");
+                        }
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
